b39431004e02fb03d1aa9267744e52baebc2bc72
Thai Le <lnthai2002@gmail.com>
2020-12-03 13:13:43 -0500

    Initial

---
+++ src/test/java/info/noip/darkportal/service/invesment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package info.noip.darkportal.service.invesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
