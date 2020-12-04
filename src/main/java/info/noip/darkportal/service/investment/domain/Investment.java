4833f506b9f2d6872805c7a83a5a09b2ec951ea8
Thai Le <lnthai2002@gmail.com>
2020-12-04 10:55:41 -0500

    instruct lombok to generate fluent setter/getter for all fields EXCEPT the createdAt and updatedAt. These fields are used by the framework so they cannot be in fluent form.

--- src/main/java/info/noip/darkportal/service/investment/domain/Investment.java
+++ src/main/java/info/noip/darkportal/service/investment/domain/Investment.java
@@ -1,7 +1,9 @@
 package info.noip.darkportal.service.investment.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -14,6 +16,7 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
+@Accessors(chain = true, fluent = true)
 public class Investment {
     @Id
     @GeneratedValue
@@ -22,8 +25,32 @@ public class Investment {
     private Long monthlyDepCents;
     private BigDecimal rate;
     private Integer months;
+
+    //these auditing fields are used by the framwwork, their setter/getter cannot be fluent.
+    //Thus I have to disable lombok on these and create standard setter/getter for them
     @CreatedDate
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
     private Long createdAt;
+
     @LastModifiedDate
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
     private Long updatedAt;
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    protected void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    protected void setUpdatedAt(Long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
