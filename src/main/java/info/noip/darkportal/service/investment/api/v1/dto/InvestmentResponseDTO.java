b863e96705199e119392e9e5f87a2dd86e3af89b
Thai Le <lnthai2002@gmail.com>
2020-12-07 14:57:03 -0500

    unused import

--- src/main/java/info/noip/darkportal/service/investment/api/v1/dto/InvestmentResponseDTO.java
+++ src/main/java/info/noip/darkportal/service/investment/api/v1/dto/InvestmentResponseDTO.java
@@ -3,9 +3,6 @@ package info.noip.darkportal.service.investment.api.v1.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.Accessors;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
